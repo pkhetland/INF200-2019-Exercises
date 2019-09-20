a6aecabf0d0d07f9fb81dced4e1a2a6df30c0e58
Bishnu Poudel <bishnu.poudel@nmbu.no>
2019-09-20 19:46:29 +0200

    Initial commit with the codes written

---
+++ src/bishnu_poudel_ex/ex02/file_stats.py
@@ -0,0 +1,32 @@
+import io
+
+
+def char_counts(text_file_name):
+    with io.open(text_file_name, 'r', encoding='utf8') as f:
+        text = f.read()
+    # print(text)
+    utf_code_list = [0] * 256
+    for i in range(len(text)):
+        utf_code = ord(text[i])
+        utf_code_list[utf_code] = utf_code_list[utf_code]+1
+    return utf_code_list
+
+
+ans = char_counts("ipconfig_all.txt")
+print(ans)
+
+if __name__ == '__main__':
+
+    filename = 'ipconfig_all.txt'
+    frequencies = char_counts(filename)
+    for code in range(256):
+        if frequencies[code] > 0:
+            character = ''
+            if code >= 32:
+                character = chr(code)
+
+            print(
+                '{:3}{:>4}{:6}'.format(
+                    code, character, frequencies[code]
+                )
+            )
