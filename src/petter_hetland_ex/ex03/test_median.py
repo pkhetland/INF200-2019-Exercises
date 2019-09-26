33e15e1d64a29b1df133b5a6d400f35b6191efe7
Petter Hetland <Pehe@nmbu.no>
2019-09-26 12:34:30 +0200

    Implemented the test_one_element_list test for the median(list) function without errors.

--- src/petter_hetland_ex/ex03/test_median.py
+++ src/petter_hetland_ex/ex03/test_median.py
@@ -20,3 +20,11 @@ def median(data):
         else 0.5 * (sdata[n // 2 - 1] + sdata[n // 2])
     )
 
+
+def test_one_element_list():
+    """"". Tests that the median function returns
+    the correct value for a one-element list.
+    """
+
+    median_5 = median([5])
+    assert median_5 == 5
