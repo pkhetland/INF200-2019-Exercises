8d612f33fb31b70fc12020a882562399fc1fdfda
Petter Hetland <Pehe@nmbu.no>
2019-09-26 12:14:44 +0200

    Added the exercise files to the ex03 folder.

---
+++ src/petter_hetland_ex/ex03/test_median.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'Petter Hetland'
+__email__ = 'pehe@nmbu.no'
+
+
+# Median function sourced from ex03.rst authored by Yngve Mardal Moe
+def median(data):
+    """
+    Returns median of data.
+
+    :param data: An iterable of containing numbers
+    :return: Median of data
+    """
+
+    sdata = sorted(data)
+    n = len(sdata)
+    return (sdata[n//2] if n % 2 == 1
+            else 0.5 * (sdata[n//2 - 1] + sdata[n//2]))
