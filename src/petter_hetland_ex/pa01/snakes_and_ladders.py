5eefac2baea2c74113a8c1d22f66e629c0bde7dd
Petter Hetland <Pehe@nmbu.no>
2019-10-08 17:52:15 +0200

    Made changes to a few variable names and doc-strings as to comply with the exercise instruction.

--- src/petter_hetland_ex/pa01/snakes_and_ladders.py
+++ src/petter_hetland_ex/pa01/snakes_and_ladders.py
@@ -5,21 +5,26 @@ __email__ = "eihelland@nmbu.no, pehe@nmbu.no"
 
 
 import random
+import statistics as s
 
 
 def add_players(n):
+    """
+    Add_players(n) makes a list of lists containing 0. Each list of 0
+    represents a player and n is the number of players in the game.
+    This will be used to store where the players are on the board
+    and track their history using append.
+    """
     player_position_list = [[0] for _ in range(n)]
     return player_position_list
 
 
-"""
-add_players(n) makes a list of lists containing 0. Each list of 0 represents a
- player and n is the number of players in the game. This will be used to store
- where the players are on the board and track their history using append.
-"""
-
-
 def snake_and_ladders():
+    """
+    Snake_and_ladders() makes a dictionary which contains all the positions
+     on the board with ladders or snakes. This will be used to promote
+     or relegate a player to the intended position.
+    """
     snake_and_ladders_dict = {
         1: 40,
         8: 10,
@@ -34,53 +39,39 @@ def snake_and_ladders():
         56: 37,
         64: 27,
         74: 12,
-        87: 70
+        87: 70,
     }
     return snake_and_ladders_dict
 
 
-"""
-snake_and_ladders() makes a dictionary which contains all the positions on the
-board with ladders or snakes. This will be used to promote or relegate a 
-player to the intended position. 
-"""
+def dice_roll():
+    """Dice_roll() tosses a 6-sided roll_value and returns the value.
+    """
+    roll_value = random.randint(1, 6)
+    return roll_value
 
 
-def dice_value():
-    dice = random.randint(1, 6)
-    return dice
+def single_game(num_players):
+    """
+    Returns duration of single game.
 
+    Arguments
+    ---------
+    num_players : int
+        Number of players in the game
 
-"""
-dice_value() tosses a 6-sided dice and returns the value. 
-"""
-
-
-def win_checker(positions, num_of_players):
-    for x in range(num_of_players):
-        for y in range(len(positions[0])):
-            if positions[x][y] >= 90:
-                return 1
-            else:
-                pass
-    return 0
-
-
-"""
-NOT USED
-win_checker() checks if the game is won, so it can end the game if somebody
-wins.
-"""
-
-
-def single_game(num_of_players):
-    positions = add_players(num_of_players)
+    Returns
+    -------
+    num_moves : int
+        Number of moves the winning player needed to reach the goal
+    """
+    positions = add_players(num_players)
     snake_ladders = snake_and_ladders()
     winner = []
     has_won = False
-    while has_won is not True:
-        for player in range(num_of_players):
-            dice = dice_value()
+    while has_won is not True:  # Runs loop until win-state
+        for player in range(num_players):
+            dice = dice_roll()
             positions[player].append(positions[player][-1] + dice)
             if positions[player][-1] in snake_ladders.keys():
                 positions[player][-1] = snake_ladders[positions[player][-1]]
@@ -88,34 +79,74 @@ def single_game(num_of_players):
                 pass
             if positions[player][-1] >= 90:
                 winner = positions[player]
-                has_won = True
+                has_won = True  # Loop is exited
             else:
                 pass
     return len(winner) - 1
 
 
-"""
-single_game(num_of_players) runs a single game and stops when one of the player
-has won. The input is how many players you want to play. The function returns
-how many turns the winner used to win.
-"""
-
-
 def multiple_games(num_games, num_players):
-    """Returns durations of a number of games.
     """
-    game_lengths = []
+    Returns durations of a number of games.
+
+    Arguments
+    ---------
+    num_games : int
+        Number of games to play
+    num_players : int
+        Number of players in the game
+
+    Returns
+    -------
+    num_moves : list
+        List with the numbedr of moves needed in each game.
+    """
+    num_moves = []
     for _ in range(num_games):
-        game_lengths.append(single_game(num_players))
-    return game_lengths
+        num_moves.append(single_game(num_players))
+    return num_moves
 
 
 def multi_game_experiment(num_games, num_players, seed):
+    """Returns durations of a number of games when playing with given seed.
+
+    Arguments
+    ---------
+    num_games : int
+        Number of games to play
+    num_players : int
+        Number of players in the game
+    seed : int
+        Seed used to initialise the random number generator
+
+    Returns
+    -------
+    num_moves : list
+        List with the numbedr of moves needed in each game.
+    """
     random.seed(seed)
-    game_lengths = []
+    num_moves = []
     for _ in range(num_games):
-        game_lengths.append(single_game(num_players))
-    return game_lengths
+        num_moves.append(single_game(num_players))
+    return num_moves
 
-print(multi_game_experiment(100, 4, 2))
 
+if __name__ == "__main__":
+    """
+    Main block runs an experiment of 100 games with 4 players and
+    a seed of 10.
+    
+    Returns
+    -------
+    - Minimum number (int) of moves among the 100 games run
+    - Maximum number (int) of moves among the 100 games run
+    - Mean value (float) of all game durations
+    - Median value (int) of all game durations
+    """
+
+    experiment = multi_game_experiment(100, 4, 10)
+
+    print(f"Minimum number of moves: {min(experiment)}")
+    print(f"Maximum number of moves: {max(experiment)}")
+    print(f"Mean value of the experiment: {s.mean(experiment)}")
+    print(f"Median value of the experiment: {s.median(experiment)}")
