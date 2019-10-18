406b9df005c5b19e63a62eaec1f427427bd73b1b
Bishnu Poudel <bishnu.poudel@nmbu.no>
2019-10-19 01:19:20 +0200

    Completed second class in myrand.  Tried the try-exception approach

--- src/bishnu_poudel_ex/ex04/myrand.py
+++ src/bishnu_poudel_ex/ex04/myrand.py
@@ -22,18 +22,36 @@ class LGCRand:
             self.input_seed= (self.a * self.input_seed) % self.m
             
 
-# class ListRand:
-#     def __init__(self):
-#         pass
-#
-#     def rand(self):
-#         pass
-#     pass
+class ListRand:
+    
+    def __init__(self, list_input):
+        self.list_input= list_input
+        self.i=0
+         
 
-#if __name__== "__main__":
-c = LGCRand(6)
-gen = c.rand()
-for i in range(5):
-    print (next(gen))
+    def rand(self):                
+        while True:
+            try:
+                yield self.list_input[self.i]
+            except Exception as e:
+                print(e)
+                return
+            else:
+                self.i += 1
+
+
+if __name__== "__main__":
+    c = LGCRand(6)
+    gen = c.rand()
+    for i in range(5):
+        print (next(gen))
+        
+    array_list=[1,2,3]
+    d= ListRand(array_list)
+    gen1 = d.rand()
+    for i in range(3):
+        print(next(gen1))
+        
+    print(next(gen1))
 
    
